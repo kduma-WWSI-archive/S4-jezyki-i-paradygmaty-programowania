2ec7d188cf65c12dfaf19d0e43429cbc5109c1fa
Krystian Duma <git@krystian.duma.sh>
2018-04-08 12:11:52 +0200

    Up

---
+++ Zjazd 4/Mapa/Mapa/Pojazd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapa
+{
+    class Pojazd
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double V { get; set; }
+        public double Kierunek { get; set; }
+        private Image obraz;
+
+        public Pojazd()
+        {
+
+        }
+    }
+}
