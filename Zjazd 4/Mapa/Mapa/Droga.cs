64629d08f17ccded667ffb94beba219d3943ef24
Krystian Duma <git@krystian.duma.sh>
2018-04-08 11:56:59 +0200

    UP

---
+++ Zjazd 4/Mapa/Mapa/Droga.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Mapa
+{
+    public class Droga
+    {
+        public List<PointF> Punkty { get; set; }
+        private readonly Pen _bialaLinia;
+        private readonly Pen _asfalt;
+
+        public Droga()
+        {
+            Punkty = new List<PointF>();
+
+            _bialaLinia = new Pen(Color.White)
+            {
+                DashStyle = DashStyle.Dash
+            };
+
+            _asfalt = new Pen(Color.DarkGray, 20)
+            {
+                StartCap = LineCap.Round,
+                LineJoin = LineJoin.Round
+            };
+        }
+
+        public void Rysuj(Graphics g)
+        {
+            g.DrawLines(_asfalt, Punkty.ToArray());
+            g.DrawLines(_bialaLinia, Punkty.ToArray());
+        } 
+
+    }
+}
